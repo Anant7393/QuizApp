706549738377c154993ecdf9bb7d0fab4b5c1c64
Anant7393 <anantmishra7393@gmail.com>
2021-07-03 16:52:14 +0530

    commit

---
+++ js/questions.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "My fav Valo character?",
+    answer: "Raze",
+    options: [
+      "Raze",
+      "Yoru",
+      "Omen",
+      "Kay/o"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The reflex angle between the hands of a clock at 10.25 is",
+    answer: "approx 197 degree",
+    options: [
+      "180 degree",
+      "135 degree",
+      "approx 197 degree",
+      " 313 degree "
+    ]
+  },
+    {
+    numb: 3,
+    question: "Prime Minister of India?",
+    answer: "Narendra Modi",
+    options: [
+      "Sarthak Rajora",
+      "Narendra Modi",
+      "Omu the Roaster",
+      "Rahul Gandhi"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A train running at the speed of 60 km/hr crosses a pole in 9 seconds. What is the length of the train?",
+    answer: "	150 metres",
+    options: [
+      "	150 metres",
+      "	321 metres",
+      "	250 metres",
+      "	75 metres"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+ 
+
+];
